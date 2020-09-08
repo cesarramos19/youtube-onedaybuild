d19716df6459419de7403a08b1870c6ed75754fa
Cesar E Ramos <personal@Cesars-MacBook-Pro.local>
2020-09-08 15:13:08 -0400

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Cesar E Ramos  on 9/8/20.
+//  Copyright © 2020 A4A, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
