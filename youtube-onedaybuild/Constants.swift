061af654cf644046e43168c9ac17249df96fe8ac
Cesar E Ramos <personal@Cesars-MacBook-Pro.local>
2020-09-08 23:10:19 -0400

    modified gitignore to stop tracking Constants.swift

---
+++ youtube-onedaybuild/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constans.swift
+//  youtube-onedaybuild
+//
+//  Created by Cesar E Ramos  on 9/8/20.
+//  Copyright © 2020 A4A, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    
+    static var API_KEY = "AIzaSyC8Ik_HKSx-fT7npkoB8Q66BbbJBJLWZpI"
+    static var PLAYLIST_ID = "OLAK5uy_kqEy0CbXf0Fn-ziLycYNO92Ph3UZVYess"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+    
+    
+    
+}
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Cesar E Ramos  on 9/8/20.
+//  Copyright © 2020 A4A, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
